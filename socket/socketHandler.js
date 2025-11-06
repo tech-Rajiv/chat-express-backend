fa093a9780babeb20c67bc22378c63bbe7148139
mac <mac@tagline-2.local>
2025-11-06 17:05:46 +0530

    added seen feature partially

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -1,9 +1,16 @@
 import { handleSeenMessage, handleSendMessage } from "./HelperSocket.js";
 
 export default async function socketHandler(io) {
+  const onlineUsers = new Map();
   io.on("connection", (socket) => {
     console.log("user connected:", socket.id);
 
+    //online feature
+    socket.on("user_connected", (userId) => {
+      onlineUsers.set(userId, socket.id);
+      console.log("online userssss", onlineUsers);
+      io.emit("online_users", Array.from(onlineUsers.keys()));
+    });
     // Join specific chat room
     socket.on("join_room", (roomKey) => {
       socket.join(roomKey);
@@ -15,6 +22,13 @@ export default async function socketHandler(io) {
     socket.on("seen_message", (data) => handleSeenMessage(io, socket, data));
 
     socket.on("disconnect", () => {
+      for (const [userId, id] of onlineUsers.entries()) {
+        if (id === socket.id) {
+          onlineUsers.delete(userId);
+          break;
+        }
+      }
+      io.emit("online_users", Array.from(onlineUsers.keys()));
       console.log("🔴 User disconnected:", socket.id);
     });
   });
